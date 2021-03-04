5203821f9d4408cd9faa115401229593aa51070b
JVSC <joaovictor.clementino@gmail.com>
2021-03-04 11:47:39 -0300

    corrige indice reverso para maneira correta

--- _invertedIndex.py
+++ _invertedIndex.py
@@ -6,29 +6,33 @@ nltk.download('punkt')
 from nltk.corpus import stopwords  
 from nltk.tokenize import word_tokenize  
 
-def count_word(sentence):
-    counter = {}
-    for word in set(sentence):
-        counter[word] = 0
-        for i in range(len(sentence) -1, -1, -1):
-            if sentence[i] == word: 
-                counter[word]=counter[word]+1
-                del sentence[i]
+def inverted_index(corpus, terms):
+    inverted_index = dict()
+    for term in terms:
+        inverted_index[term] = list()
+        for index, tweet in enumerate(corpus):
+            if term in tweet['tokenized']:
+                inverted_index[term].append(index)
 
-    return sorted(counter.items(), key=lambda item:item[1])
-    print("-")
+    return inverted_index
 
 def parse_corpus():
-    tweet_arr = []
+    corpus = []
     stop_words = set(stopwords.words('english'))
-
+    stop_words.update(set(stopwords.words('portuguese')))
+    
     with open("./twitter_corpus/full-corpus.csv") as file:
         data = csv.reader(file)
-        doc = {}
+        terms = set()
         next(data)
         for tweet in data:
+            doc = {}
             doc['topico'], doc['sentimento'], doc['tweet_id'], doc['tweet_horario'], doc['texto'] = tweet
-            doc['tokenized'] = [w.lower() for w in word_tokenize(doc['texto']) if not w in stop_words]
-            doc['indexed'] = count_word(doc['tokenized'])
-            tweet_arr.append(doc)
-    return tweet_arr
+            doc['tokenized'] = set([w.lower() for w in word_tokenize(doc['texto']) if not w in stop_words])
+            corpus.append(doc)
+            terms.update(doc['tokenized'])
+
+    return {
+        'termos': terms,
+        'corpus': corpus
+    }
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import nltk
-from _invertedIndex import parse_corpus
+from _invertedIndex import parse_corpus, inverted_index
 
-print(len(parse_corpus()))
+docs = parse_corpus()
+index = inverted_index(docs['corpus'], docs['termos'])
+
+for i in index:
+    print(f'indice: {i} - {index[i]}')

3a6bda43dd1d116cbf43055c27259abc478d6a12
Gabriel <gabrielgarciapavan@outlook.com>
2021-03-04 23:21:35 -0300

    Gabriel - Criação de função de stemming e construção de index invertido após stemming

--- _invertedIndex.py
+++ _invertedIndex.py
@@ -4,7 +4,8 @@ nltk.download('stopwords')
 nltk.download('punkt')
 
 from nltk.corpus import stopwords  
-from nltk.tokenize import word_tokenize  
+from nltk.tokenize import word_tokenize 
+from nltk.stem import PorterStemmer 
 
 def inverted_index(corpus, terms):
     inverted_index = dict()
@@ -13,7 +14,6 @@ def inverted_index(corpus, terms):
         for index, tweet in enumerate(corpus):
             if term in tweet['tokenized']:
                 inverted_index[term].append(index)
-
     return inverted_index
 
 def parse_corpus():
@@ -21,7 +21,7 @@ def parse_corpus():
     stop_words = set(stopwords.words('english'))
     stop_words.update(set(stopwords.words('portuguese')))
     
-    with open("./twitter_corpus/full-corpus.csv") as file:
+    with open("./twitter_corpus/full-corpus.csv", encoding='utf8') as file:
         data = csv.reader(file)
         terms = set()
         next(data)
@@ -36,3 +36,23 @@ def parse_corpus():
         'termos': terms,
         'corpus': corpus
     }
+
+def stemming_tokens(words):
+    stemming = []
+    stemming_map = []
+    ps = PorterStemmer()
+    for w in words:
+        doc = {}
+        doc['original'], doc['stemming'] = w, ps.stem(w)
+        stemming_map.append(doc)
+        stemming.append(ps.stem(w))
+    return stemming, stemming_map
+
+def inverted_index_stemming(corpus, terms):
+    inverted_index = dict()
+    for term in terms:
+        inverted_index[term] = list()
+        for index, tweet in enumerate(corpus):
+            if term in tweet['texto']:
+                inverted_index[term].append(index)
+    return inverted_index
--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-import nltk
-from _invertedIndex import parse_corpus, inverted_index
+
+from _invertedIndex import parse_corpus
+from _invertedIndex import inverted_index
+from _invertedIndex import stemming_tokens
+from _invertedIndex import inverted_index_stemming
 
 docs = parse_corpus()
 index = inverted_index(docs['corpus'], docs['termos'])
+stemming,stemming_map = stemming_tokens(docs['termos'])
+index_stemming = inverted_index_stemming(docs['corpus'], stemming)
+
+file_index = open('index_invertido.txt', 'w', encoding='utf8')
+file_index_stemming = open('index_invertido_stemming.txt', 'w',encoding='utf8')
+file_stemming = open('stemming.txt', 'w',encoding='utf8')
 
 for i in index:
-    print(f'indice: {i} - {index[i]}')
+    file_index.write(f'indice(tamanho): {i}({len(index[i])}) - {index[i]}\n')
+    #     print(f'indice: {i} - {index[i]}')
+
+for i in index_stemming:
+    file_index_stemming.write(f'indice(tamanho): {i}({len(index_stemming[i])}) - {index_stemming[i]}\n')
+    #     print(f'indice: {i} - {index_stemming[i]}')
+    
+file_stemming.write('Token Original - Token com Stemming')
+for s in stemming_map:
+    file_stemming.write(f'{s["original"]} - {s["stemming"]}\n')
+    #     print(f'{s["original"]} - {s["stemming"]}')
+    
+file_index.close()
+file_index_stemming.close()
+file_stemming.close()

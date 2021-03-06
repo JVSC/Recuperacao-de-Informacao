a7953705ac5b304d3568e32c284aa703d4774ec4
Gabriel <gabrielgarciapavan@outlook.com>
2021-03-06 01:03:17 -0300

    Gabriel - Algoritmo indexando todo o Corpus

--- ri.py
+++ ri.py
@@ -7,6 +7,11 @@ from nltk.tokenize import word_tokenize
 from nltk.tokenize import sent_tokenize
 from nltk.stem import RSLPStemmer
 
+from os import listdir
+from os.path import isfile, join
+
+mypath = 'C:\\Users\\Gabriel\\Desktop\\Algoritmo - Recuperação da Informação\\Obras'
+
 def rem_dup(seq, idfun=None): 
    if idfun is None:
        def idfun(x): return x
@@ -19,14 +24,24 @@ def rem_dup(seq, idfun=None):
        result.append(item)
    return result
 
+temporario = []
+
 def inverted_index(sentencas_processadas, terms):
     inverted_index = dict()
     for term in terms:
-        inverted_index[term] = list()
+        inverted_index[term] = {"obras":[], "sentencas": []}
         for index, sentenca in enumerate(sentencas_processadas):
-            if term in sentenca:
-                inverted_index[term].append(index)
+            for i, s in enumerate(sentenca['sentencas']):                
+                if term in s:
+                    inverted_index[term]["obras"].append(sentenca['id'])
+                    inverted_index[term]["sentencas"].append((sentenca['id'], i))
     return inverted_index
+
+def rem_dup_index(inverted_index, terms):
+    for term in terms:
+        inverted_index[term]['obras'] = list(dict.fromkeys(inverted_index[term]['obras']))
+    return inverted_index
+    
 
 nltk.download('machado')
 nltk.download('punkt')
@@ -34,45 +49,81 @@ nltk.download('stopwords')
 nltk.download('rslp')
 
 #documento = nltk.corpus.machado.raw('romance/marm08.txt')
-arquivo = open('Dom Casmurro - MA.txt', 'r')
-documento = arquivo.read()
+documentos = []
+
+print('Começo de leitura de arquivos...')
+
+files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+for index, file in enumerate(files):
+    arquivo = open(f'Obras/{file}', 'r', encoding='unicode_escape')
+    documentos.append({"id": index, "obra": arquivo.read()})
+    arquivo.close()
+
+print('Final de leitura de arquivos...')
+
 stopwords = set(nltk.corpus.stopwords.words('portuguese'))
 
-sentencas_processadas = sent_tokenize(documento.replace('\n', ' '))
+print('Processando sentenças...')
+
+sentencas_processadas = []
+for index, documento in enumerate(documentos):
+    sentencas_processadas.append({"id": index, "sentencas": sent_tokenize(documento['obra'].replace('\n', ' '))})
+
+print('Finalizado processamento de sentenças...')
+
+print('Iniciando tokenização...')
 
 tokens = []
-for s in sentencas_processadas:
-    words = word_tokenize(s)    
-    for w in words:
-        tokens.append(w)
+for sentenca in sentencas_processadas:
+    for s in sentenca["sentencas"]:
+        words = word_tokenize(s)    
+        for w in words:
+            tokens.append(w)
 
 tokens = [t for t in tokens if t.isalnum()]
 tokens_lower = [t.lower() for t in tokens]
 tokens_stepwords = [w for w in tokens_lower if w not in stopwords]
 
+print('Finalizado tokenização...')
+print('Removendo redundancias...')
+
 vocab = rem_dup(tokens_stepwords)
 
-index = inverted_index(sentencas_processadas, vocab)
+print('Remoção de redundancias concluídas...')
 
+print('Iniciando indexação...')
+
+index = inverted_index(sentencas_processadas, vocab)
+index = rem_dup_index(index, vocab)
+
+print('Finalizado indexação...')
+
+print('Iniciando processo de Stemming...')
 stemmer = RSLPStemmer()
 tokens_stemming = [stemmer.stem(t) for t in vocab]
 tokens_after_stemming = rem_dup(tokens_stemming)
 
+print('Finalizado processo de Stemming...')
+print('Indexando após Stemming...')
+
 index_stemming = inverted_index(sentencas_processadas, tokens_after_stemming)
+index_stemming = rem_dup_index(index_stemming, tokens_after_stemming)
 
-file_index = open('Index Invertido.txt', 'w', encoding='utf8')
-file_index_stemming = open('Index Inv. com Stemming.txt', 'w', encoding='utf8')
+print('Finzalizado processo após Stemming...')
 
-for i in index:
-    file_index.write(f'index(size): {i}({len(index)}) -> {index[i]}\n')
+#file_index = open('Index Invertido.txt', 'w', encoding='utf8')
+# file_index_stemming = open('Index Inv. com Stemming.txt', 'w', encoding='utf8')
+
+# for i in index:
+#     file_index.write(f'index(size): {i}({len(index)}) -> {index[i]}\n')
     
-for i in index_stemming:
-    file_index_stemming.write(f'index(size): {i}({len(index_stemming)}) -> {index_stemming[i]}\n')
+# for i in index_stemming:
+#     file_index_stemming.write(f'index(size): {i}({len(index_stemming)}) -> {index_stemming[i]}\n')
 
 
 arquivo.close()
-file_index.close()
-file_index_stemming.close()
+#file_index.close()
+# file_index_stemming.close()
 
 
 

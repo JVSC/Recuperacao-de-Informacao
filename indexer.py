90e4b087bd42447e00912f867c0c97bd59ec33ef
JVSC <joaovictor.clementino@gmail.com>
2021-03-07 02:10:58 -0300

    reorganiza projeto e refatora algumas partes do codigo

---
+++ indexer.py
@@ -0,0 +1,77 @@
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem import RSLPStemmer
+
+from os import listdir
+from os.path import isfile, join
+
+from helpers import remove_duplicatas
+
+class Indexer:
+    # carrega os documentos para memória
+    def __init__(self,link_documentos):
+        # documentos completos
+        self.documentos = []
+        # documentos tokenizado em frases
+        self.sentencas_processadas = []
+        # tokens
+        self.tokens = []
+        self.tokens_stemmed = []
+        # vocabulario unico
+        self.vocabulario = []
+
+        self.carregar_documentos(link_documentos)
+        self.processar_sentencas()
+
+    def carregar_documentos(self, link_documentos):
+        files = [f for f in listdir(link_documentos) if isfile(join(link_documentos, f))]
+        print(f'- Carregando documentos')
+        for index, file in enumerate(files):
+            with open(f'Obras/{file}', 'r', encoding='unicode_escape') as arquivo:
+                self.documentos.append({"id": index, "obra": arquivo.read()})
+
+    def processar_sentencas(self):
+        print("Processando sentenças")
+        for index, documento in enumerate(self.documentos):
+            self.sentencas_processadas.append({"id": index, "textos": sent_tokenize(documento['obra'].replace('\n', ' ').lower())})
+
+    def tokenizar(self, stem):
+        print("Tokenizando...")
+        stopwords = nltk.corpus.stopwords.words('portuguese')
+
+        for sentenca in self.sentencas_processadas:
+            stemmer = None
+            if(stem): 
+                stemmer = RSLPStemmer()
+
+            for texto in sentenca["textos"]:
+                palavras = word_tokenize(texto)    
+                for palavra in palavras:
+                    if palavra in stopwords or not palavra.isalnum():
+                        continue
+                    palavra = palavra.lower()
+                    if(stem):
+                        palavra = stemmer.stem(palavra)
+                    self.tokens.append(palavra)
+
+        self.vocabulario = remove_duplicatas(self.tokens)
+
+    def inverted_index(self, stem):
+        self.tokenizar(stem)
+        inverted_index = dict()
+
+        for term in self.tokens:
+
+            inverted_index[term] = {"obras":[], "sentencas": []}
+            for index, sentenca in enumerate(self.sentencas_processadas):
+                for i, texto in enumerate(sentenca['textos']):                
+                    if term in texto:
+                        inverted_index[term]["obras"].append(sentenca['id'])
+                        inverted_index[term]["sentencas"].append((sentenca['id'], i))
+
+            if sum(inverted_index[term]['obras']) == 0.0:
+                del inverted_index[term]
+            else:
+                inverted_index[term]['obras'] = list(dict.fromkeys(inverted_index[term]['obras']))
+
+        return inverted_index
---
+++ matrizes.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import seaborn as sns
+from os import listdir
+import math
+import json
+
+mypath = "./Obras"
+
+def frequency_terms_matrix(inverted_index):
+    frequency_matrix = dict()
+    for term in inverted_index:
+        frequency_matrix_doc = [0] * len(listdir(mypath))
+        for sentenca in inverted_index[term]['sentencas']:
+            frequency_matrix_doc[sentenca[0]] += 1
+        frequency_matrix[term] = frequency_matrix_doc
+    return frequency_matrix
+            
+def wtd_matrix(frequency_matrix):
+    wtd_matrix = dict()
+    for term in frequency_matrix:
+        wtd_matrix_docs = [0] * len(listdir(mypath))
+        for index, freq in enumerate(frequency_matrix[term]):
+            if freq != 0:
+                wtd_matrix_docs[index] = 1 + math.log(freq, 10)
+        wtd_matrix[term] = wtd_matrix_docs
+    return wtd_matrix
+
+def tf_query_document(query, doc, tf_terms_matrix):
+    pass
+
+def idf_matrix(frequency_matrix):
+    idf_matrix = dict()
+    for term in frequency_matrix:
+        idf_matrix[term] = math.log(len(listdir(mypath))/len([i for i in frequency_matrix[term] if i > 0]), 10)
+    return idf_matrix
+
+def tf_idf_matrix(frequency_matrix):
+    tf_idf_matrix = dict()
+    for term in frequency_matrix:
+        tf_idf_matrix_docs = [0] * len(listdir(mypath))
+        for index in range(len(listdir(mypath))):
+            if frequency_matrix[term][index] >= 1:
+                tf_idf_matrix_docs[index] = (1 + math.log(frequency_matrix[term][index], 10)) * math.log(len(listdir(mypath))/len([i for i in frequency_matrix[term] if i > 0]), 10)
+        tf_idf_matrix[term] = tf_idf_matrix_docs
+    return tf_idf_matrix
+
+def build_matrizes():
+    index_stemming = open('./indexed/index_stemmed.json', 'r', encoding='utf8')
+    index_json = json.load(index_stemming)
+    index_stemming.close()
+
+    frequency_matrix = frequency_terms_matrix(index_json)
+    wtd_matrix_var = wtd_matrix(frequency_matrix)
+    idf_matrix_var = idf_matrix(frequency_matrix)
+    tf_idf_matrix_var = tf_idf_matrix(frequency_matrix)
+
+    with open('./scoring/frequency_matrix.json', 'w', encoding='utf8') as fp:
+        json.dump(frequency_matrix, fp)
+        
+    with open('./scoring/wtd_matrix.json', 'w', encoding='utf8') as fp:
+        json.dump(wtd_matrix_var, fp)
+        
+    with open('./scoring/idf_matrix.json', 'w', encoding='utf8') as fp:
+        json.dump(idf_matrix_var, fp)
+        
+    with open('./scoring/tf_idf_matrix.json', 'w', encoding='utf8') as fp:
+        json.dump(tf_idf_matrix_var, fp)
+
+    terms_frequency = []
+    for term in frequency_matrix:
+        terms_frequency.append(term)
+
+    terms_tf_idf = []
+    for term in tf_idf_matrix_var:
+        terms_tf_idf.append(term)
+
+    df_frequency_matrix = pd.DataFrame (frequency_matrix, columns = terms_frequency)
+    df_tf_idf_matrix = pd.DataFrame (tf_idf_matrix_var, columns = terms_tf_idf)
+
+    sns.set()
+    #parametros para definir min e max de escala de valores: vmin=0, vmax=0.5
+    #ax1 = sns.heatmap(df_frequency_matrix)
+    #ax2 = sns.heatmap(df_tf_idf_matrix) 
